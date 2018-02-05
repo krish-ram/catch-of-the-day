4b75d6b9d1bd6d3c9f062d3a375203e958353fdf
Ram Krishna <krishram101@gmail.com>
2018-02-05 06:24:54 +0530

    adding src files

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass ({
+	apiKey: "AIzaSyBnXhNetS5pO8mJ2pO8Hcz2ROo2GrAI23A",
+    authDomain: "catch-of-the-day-ram.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-ram.firebaseio.com"
+});
+
+export default base;
